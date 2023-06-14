8ae9e24759cfe9d9cb034d23da5fb1361bf91400
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-06-14 21:10:55 +0530

    added instance

--- instance.tf
+++ instance.tf
@@ -3,7 +3,29 @@
 resource "aws_instance" "ec2instance" {
   ami = "ami-022e1a32d3f742bd8"
   instance_type = "t2.micro"
-  tags = {
-     "Name"="Pipelineec2Instance"
-  }
+
+
+
+  # Security Group
+  vpc_security_group_ids = ["sg-0693bcac47a3c7f04"]  # Replace with your security group ID
+
+  # Key Pair
+  key_name               = "pipelinepair"  # Replace with your key pair name
+
+  # User Data
+  user_data = <<-EOF
+    #!/bin/bash
+    # Your user data script to configure the EC2 instance
+    # Install necessary tools, configure environment, etc.
+    # ...
+
+    # Clone the repository and deploy the application
+    git clone <https://github.com/PradeepaLakshmanan-CH-2022/PipelineRepository> /path/to/repository
+    # Run necessary commands to set up and run your application
+    # ...
+
+    # Start your application
+    cd /path/to/repository
+    ./start.sh  # Replace with the command to start your application
+  EOF
 }
--- output.tf
+++
@@ -1,6 +0,0 @@
-output "website_url" {
-    description = "Website Url"
-
-    value=aws_s3_bucket_website_configuration
-  
-}
