6e6acd582c3772aabcd379fbf681b7d69acf9ee2
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-06-15 11:06:11 +0530

    add pipeline

--- pipeline.tf
+++ pipeline.tf
@@ -23,7 +23,30 @@ resource "aws_codebuild_project" "tf-plan" {
  }
 }
 
+resource "aws_codebuild_project" "tf-apply" {
+  name          = "tf-cicd-apply"
+  description   = "Apply stage for terraform"
+  service_role  = aws_iam_role.tf-codebuild-role.arn
 
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "hashicorp/terraform:1.4.6"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+    registry_credential{
+        credential = var.dockerhub_credentials
+        credential_provider = "SECRETS_MANAGER"
+    }
+ }
+ source {
+     type   = "CODEPIPELINE"
+     buildspec = file("buildspec/apply-buildspec.yml")
+ }
+}
 resource "aws_codedeploy_app" "tf_cicd_app" {
   name = "tf-cicd-app"
 }
