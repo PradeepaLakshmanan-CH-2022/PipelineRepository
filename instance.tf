2b9aed5914cd449009912d3c6d585c9e66229d6b
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-06-16 10:57:27 +0530

    add in

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "pipelineinstance" {
   ami           = "ami-022e1a32d3f742bd8"
   instance_type = "t2.micro"
   key_name      = "pipelineec2"
-  vpc_security_group_ids = ["sg-09d4bee1e71ddbb9c"]
+ 
   tags = {
     Name = "ConsoleEc2"
     environment = "production"
