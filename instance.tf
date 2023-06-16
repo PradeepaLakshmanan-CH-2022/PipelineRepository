0ebb5ba56a4528bb8403582d7e6fd13429a74ea6
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-06-16 10:59:50 +0530

    add instance

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "pipelineinstance" {
   ami           = "ami-022e1a32d3f742bd8"
   instance_type = "t2.micro"
-  key_name      = "pipelineec2"
+
  
   tags = {
     Name = "ConsoleEc2"
