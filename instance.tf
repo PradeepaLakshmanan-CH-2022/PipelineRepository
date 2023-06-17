db37d12daa187c1692757f52c51456c23c5218e2
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-06-17 19:28:05 +0530

    ad in

--- instance.tf
+++ instance.tf
@@ -3,16 +3,13 @@ resource "aws_instance" "pipelineinstance" {
   ami           = "ami-04132f301c3e4f138"
   instance_type = "t2.micro"
 
- user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y ruby wget
-    cd /home/ec2-user
-    wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-    chmod +x ./install
-    ./install auto
-    service codedeploy-agent start
-    chkconfig codedeploy-agent on
+   user_data = <<-EOF
+    <powershell>
+    $url = "https://aws-codedeploy-{region}.s3.{region}.amazonaws.com/latest/codedeploy-agent.msi"
+    $output = "$env:TEMP\codedeploy-agent.msi"
+    (New-Object System.Net.WebClient).DownloadFile($url, $output)
+    Start-Process -Wait -FilePath msiexec -ArgumentList /i, $output, /quiet
+    </powershell>
   EOF
  
   tags = {
